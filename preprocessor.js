048ab342ad5b65667040704bd319df6d73106adc
Jonas Gann <jonas@gann.digital>
2023-03-30 15:17:02 +0200

    feat: add support for hierarchical content strings

--- preprocessor.js
+++ preprocessor.js
@@ -1,67 +1,88 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const { parse, find, replace, generate, Identifier, } = require("abstract-syntax-tree");
+const { parse, replace, generate, Identifier, } = require("abstract-syntax-tree");
 const jmespath_1 = __importDefault(require("jmespath"));
-const prompts = require("prompts");
 const data = require("./data.json");
+const tags = require("./tags.json");
+// Query ################################
+const query = {
+    content: "content:x.edu.de.higher.degree",
+    filter: '("language:de" && !"language:en")',
+};
+// ######################################
 /**
- * Preprocessor for translation of Enmeshed querying syntax
+ * List all content strings which are part of the subtree which has the query content string as root
  */
-const query = '("language:de" && !"language:en") && "mime:application/json" && ("schema:xschule" || "schema:elmo")';
-const fileMetadataPathMapper = (literal) => {
+const getContentOptions = (content) => {
+    const contents = Object.keys(tags.content);
+    const contentOptions = contents.filter((value) => value.startsWith(content));
+    return contentOptions;
+};
+// Translate keywords to jmespath statements
+const pathMapper = (literal) => {
     const [category, value] = literal.split(":");
     switch (category) {
+        case "content":
         case "language":
             return `contains(tags, '${literal}')`;
         case "mime":
             return `contains(structures[].mimetype[], '${value}')`;
         case "schema":
             return `contains(structures[].schemas[], '${value}')`;
-        case "content":
-            return `contains(content, '${value}')`;
     }
 };
-const jmespath_translator = (query, pathMapper) => {
-    const tree = parse(query);
+const jmespathQueryConstruction = (query) => {
+    const contentOptions = getContentOptions(query.content);
+    let contentQuery = "";
+    for (const [index, content] of contentOptions.entries()) {
+        const mapped = pathMapper(content);
+        contentQuery += mapped;
+        contentQuery += contentOptions.length - 1 == index ? "" : " || ";
+    }
+    const tree = parse(query.filter);
     replace(tree, (node) => {
         if (node.type === "Literal") {
             return new Identifier({ name: pathMapper(node.value) });
         }
     });
-    const jmespath_query = generate(tree);
-    const jmespath_query_final = `[?${jmespath_query.slice(0, -2)}].id`;
-    return jmespath_query_final;
+    const jmespath_filter = generate(tree).slice(0, -2);
+    const query_combined = `(${contentQuery}) && (${jmespath_filter})`;
+    const query_final = `[?${query_combined}].id`;
+    return query_final;
 };
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield prompts({
-        type: "text",
-        name: "query",
-        message: "Write query",
-        validate: (query) => {
-            try {
-                const jmespath_query = jmespath_translator(query, fileMetadataPathMapper);
-                jmespath_1.default.search(data, jmespath_query);
-                return true;
-            }
-            catch (err) {
-                return "Error";
-            }
-        },
+// Compute distance of content strings
+// Returns Infinity if root not parent of child
+const contentDist = (root, child) => {
+    if (!child.startsWith(root)) {
+        return Infinity;
+    }
+    else if (root == child) {
+        return 0;
+    }
+    else {
+        child = child.replace(root + ".", "");
+        const distance = child.split(".").length;
+        return distance;
+    }
+};
+const jmespathQuery = jmespathQueryConstruction(query);
+const res = jmespath_1.default.search(data, jmespathQuery);
+const getContent = (id) => {
+    const attributes = jmespath_1.default.search(data, `[?id == \`${id}\`]`);
+    if (attributes.length == 0)
+        return ""; // id does not exist
+    return attributes[0].tags.find((elem) => elem.startsWith("content:")); // should only contain one content string
+};
+const rankIDs = (query, ids) => {
+    const idsMap = ids.map((id) => {
+        return { id, dist: contentDist(query.content, getContent(id)) };
     });
-    const jmespath_query = jmespath_translator(response.query, fileMetadataPathMapper);
-    const res = jmespath_1.default.search(data, jmespath_query);
-    console.log("JMESPath Query:", jmespath_query);
-    console.log("Query Response:", res);
-}))();
+    idsMap.sort((a, b) => a.dist - b.dist);
+    return idsMap;
+};
+const result = rankIDs(query, res);
+console.log("JMESPath Query:", jmespathQuery);
+console.log("Query Response:", result);
--- preprocessor.ts
+++ preprocessor.ts
@@ -1,72 +1,120 @@
 const {
   parse,
-  find,
   replace,
   generate,
   Identifier,
 } = require("abstract-syntax-tree");
 import jmespath from "jmespath";
-const prompts = require("prompts");
 const data = require("./data.json");
+const tags = require("./tags.json");
+
+// Query ################################
+
+const query = {
+  content: "content:x.edu.de.higher.degree",
+  filter: '("language:de" && !"language:en")',
+};
+
+// ######################################
 
 /**
- * Preprocessor for translation of Enmeshed querying syntax
+ * List all content strings which are part of the subtree which has the query content string as root
  */
+const getContentOptions = (content: string): string[] => {
+  const contents: string[] = Object.keys(tags.content);
+  const contentOptions = contents.filter((value: string) =>
+    value.startsWith(content)
+  );
+  return contentOptions;
+};
 
-const query =
-  '("language:de" && !"language:en") && "mime:application/json" && ("schema:xschule" || "schema:elmo")';
-
-const fileMetadataPathMapper = (literal: string) => {
+/**
+ * Translate keywords to jmespath statements
+ */
+const pathMapper = (literal: string) => {
   const [category, value] = literal.split(":");
   switch (category) {
+    case "content":
     case "language":
       return `contains(tags, '${literal}')`;
     case "mime":
       return `contains(structures[].mimetype[], '${value}')`;
     case "schema":
       return `contains(structures[].schemas[], '${value}')`;
-    case "content":
-      return `contains(content, '${value}')`;
   }
 };
 
-const jmespath_translator = (query: string, pathMapper: Function) => {
-  const tree = parse(query);
+const jmespathQueryConstruction = (query: {
+  content: string;
+  filter: string;
+}): string => {
+  const contentOptions = getContentOptions(query.content);
+
+  let contentQuery = "";
+
+  for (const [index, content] of contentOptions.entries()) {
+    const mapped = pathMapper(content);
+    contentQuery += mapped;
+    contentQuery += contentOptions.length - 1 == index ? "" : " || ";
+  }
+
+  const tree = parse(query.filter);
   replace(tree, (node: any) => {
     if (node.type === "Literal") {
       return new Identifier({ name: pathMapper(node.value) });
     }
   });
-  const jmespath_query = generate(tree);
-  const jmespath_query_final = `[?${jmespath_query.slice(0, -2)}].id`;
-  return jmespath_query_final;
+  const jmespath_filter = generate(tree).slice(0, -2);
+
+  const query_combined = `(${contentQuery}) && (${jmespath_filter})`;
+  const query_final = `[?${query_combined}].id`;
+  return query_final;
 };
 
-(async () => {
-  const response = await prompts({
-    type: "text",
-    name: "query",
-    message: "Write query",
-    validate: (query: string) => {
-      try {
-        const jmespath_query = jmespath_translator(
-          query,
-          fileMetadataPathMapper
-        );
-        jmespath.search(data, jmespath_query);
-        return true;
-      } catch (err: any) {
-        return "Error";
-      }
-    },
+/**
+ * Compute distance of content strings
+ */
+const contentDist = (root: string, child: string) => {
+  if (!child.startsWith(root)) {
+    return Infinity;
+  } else if (root == child) {
+    return 0;
+  } else {
+    child = child.replace(root + ".", "");
+    const distance = child.split(".").length;
+    return distance;
+  }
+};
+
+/**
+ * Get content string by attribute id
+ */
+const getContent = (id: number): string => {
+  const attributes = jmespath.search(data, `[?id == \`${id}\`]`);
+  if (attributes.length == 0) return ""; // id does not exist
+  return attributes[0].tags.find((elem: string) => elem.startsWith("content:")); // should only contain one content string
+};
+
+/**
+ * Sort attributes by distance to queried content string
+ */
+const rankIDs = (
+  query: { content: string; filter: string },
+  ids: number[]
+): { id: number; dist: number }[] => {
+  const idsMap = ids.map((id) => {
+    return { id, dist: contentDist(query.content, getContent(id)) };
   });
+  idsMap.sort((a, b) => a.dist - b.dist);
+  return idsMap;
+};
 
-  const jmespath_query = jmespath_translator(
-    response.query,
-    fileMetadataPathMapper
-  );
-  const res = jmespath.search(data, jmespath_query);
+// Execution
 
-  console.log("JMESPath Query:", jmespath_query);
-  console.log("Query Response:", res);
-})();
+const jmespathQuery = jmespathQueryConstruction(query);
+const res: number[] = jmespath.search(data, jmespathQuery);
+
+const result = rankIDs(query, res);
+
+console.log("JMESPath Query:", jmespathQuery);
+console.log("Query Response:", result);
